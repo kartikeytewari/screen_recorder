03866a5f8107289f04a46b74e3d5278aca526a29
Kartikey Tewari <kartikeya30@gmail.com>
2020-12-02 20:00:04 +0530

    Added Buttons

---
+++ src/render.js
@@ -0,0 +1,7 @@
+// Getting all elements
+const video_button = document.querySelector('video');
+const start_button = document.getElementById('start_button');
+const stop_button = document.getElementById('stop_button');
+const video_selection_button = document.getElementById('video_select_button');
+
+// Getting available video resources
